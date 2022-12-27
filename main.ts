e3f7030ac8fec07237a20ff4c84974fcd2f65484
Anituk <120564870+Anituk@users.noreply.github.com>
2022-12-27 12:21:00 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,8 +8,14 @@ input.onButtonPressed(Button.B, function () {
 basic.forever(function () {
     sonar = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
     if (sonar > 2 && sonar < 15) {
-    	
+        cuteBot.motors(0, 0)
+        for (let index = 0; index < 4; index++) {
+            music.playTone(880, music.beat(BeatFraction.Quarter))
+        }
+        basic.pause(2000)
+        cuteBot.motors(randint(-50, -100), 0)
+        basic.pause(500)
     } else {
-    	
+        cuteBot.forward()
     }
 })
